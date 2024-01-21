c9afee9010408a84a69e9738061e432020668b99
Tim Büsdorf <tim090503@gmail.com>
2024-01-21 13:27:05 +0100

    Refactor schachFiguren package

--- src/main/java/schach/projekt/schachFiguren/figuren/Koenig.java
+++ src/main/java/schach/projekt/schachFiguren/figuren/Koenig.java
@@ -1,13 +1,68 @@
 package schach.projekt.schachFiguren.figuren;
 
+import schach.projekt.GameManager.Rochade;
+import schach.projekt.Schachbrett.Schachbrett;
 import schach.projekt.feld.Feld;
 import schach.projekt.schachFiguren.Schachfiguren;
 import schach.projekt.schachFiguren.Teams;
+import schach.projekt.GameManager.GameManager;
+
+import java.util.ArrayList;
 
 public class Koenig extends Schachfiguren {
 
 
     public Koenig(Teams team, Feld startFeld) {
         super("schachfiguren/" + team.name().toLowerCase() + "_king.svg", team, startFeld);
+    }
+
+    public ArrayList<Feld> getGueltigenZug() {
+        Integer[] vektoren = getVektoren();
+        ArrayList<Feld> gueltigerZug = new ArrayList<>();
+        int jetzigesFeldIndex = this.getJetzigesFeld().getFeldId();
+        for (Integer vektor : vektoren) {
+            int neuerFeldIndex = jetzigesFeldIndex + vektor;
+            if (neuerFeldIndex < 0 || neuerFeldIndex > 63) continue;
+            if (Schachbrett.felder[neuerFeldIndex].getFigurAufFeld() != null && Schachbrett.felder[neuerFeldIndex].getFigurAufFeld().getTeam() == this.getTeam())
+                continue;
+            gueltigerZug.add(Schachbrett.felder[neuerFeldIndex]);
+        }
+        gueltigerZug.addAll(getRochade());
+        return gueltigerZug;
+    }
+
+    public ArrayList<Feld> getRochade(){
+        ArrayList<Feld> rochade = new ArrayList<>();
+        //kurze Rochade
+        try {
+            Turm turm = ((Turm) Schachbrett.felder[this.getJetzigesFeld().getFeldId() + 3].getFigurAufFeld());
+            if(Schachbrett.felder[this.getJetzigesFeld().getFeldId() + 1].getFigurAufFeld() != null || Schachbrett.felder[this.getJetzigesFeld().getFeldId() + 2].getFigurAufFeld() != null) throw new Exception();
+            Feld neueKoenigPos = Schachbrett.felder[this.getJetzigesFeld().getFeldId() + 2];
+            Feld neueTurmPos = Schachbrett.felder[neueKoenigPos.getFeldId() - 1];
+            rochade.add(neueKoenigPos);
+            GameManager.addRochade(new Rochade(neueKoenigPos, neueTurmPos, turm));
+        }catch (Exception ignored){}
+        //lange Rochade
+        try {
+            Turm turm = ((Turm) Schachbrett.felder[this.getJetzigesFeld().getFeldId() - 4].getFigurAufFeld());
+            if(Schachbrett.felder[this.getJetzigesFeld().getFeldId() - 1].getFigurAufFeld() != null || Schachbrett.felder[this.getJetzigesFeld().getFeldId() - 2].getFigurAufFeld() != null || Schachbrett.felder[this.getJetzigesFeld().getFeldId() - 3].getFigurAufFeld() != null) throw new Exception();
+            Feld neueKoenigPos = Schachbrett.felder[this.getJetzigesFeld().getFeldId() - 2];
+            Feld neueTurmPos = Schachbrett.felder[neueKoenigPos.getFeldId() + 1];
+            rochade.add(neueKoenigPos);
+            GameManager.addRochade(new Rochade(neueKoenigPos, neueTurmPos, turm));
+        }catch (Exception ignored){}
+
+        return rochade;
+    }
+
+
+    private Integer[] getVektoren() {
+        Feld jetzigesFeld = this.getJetzigesFeld();
+        if (jetzigesFeld.getSpalte() == 7) {
+            return new Integer[]{-1, -8, 8, -9, 7};
+        }else if (jetzigesFeld.getSpalte() == 0) {
+            return new Integer[]{1, -8, 8, -7, 9};
+        }
+        return new Integer[]{-1, 1, -8, 8, -9, -7, 9, 7};
     }
 }
--- src/main/java/schach/projekt/schachFiguren/figuren/Springer.java
+++ src/main/java/schach/projekt/schachFiguren/figuren/Springer.java
@@ -1,11 +1,50 @@
 package schach.projekt.schachFiguren.figuren;
 
+import schach.projekt.Schachbrett.Schachbrett;
 import schach.projekt.feld.Feld;
 import schach.projekt.schachFiguren.Schachfiguren;
 import schach.projekt.schachFiguren.Teams;
+
+import java.util.ArrayList;
 
 public class Springer extends Schachfiguren {
     public Springer(Teams team, Feld startFeld) {
         super("schachfiguren/" + team.name().toLowerCase() + "_knight.svg", team, startFeld);
     }
+
+    @Override
+    public ArrayList<Feld> getGueltigenZug() {
+        Integer[] vektoren = getVektoren();
+        ArrayList<Feld> gueltigerZug = new ArrayList<>();
+        int jetzigesFeldIndex = this.getJetzigesFeld().getFeldId();
+        for (Integer vektor : vektoren) {
+            int neuerFeldIndex = jetzigesFeldIndex + vektor;
+            if (neuerFeldIndex < 0 || neuerFeldIndex > 63) continue;
+            if (Schachbrett.felder[neuerFeldIndex].getFigurAufFeld() != null && Schachbrett.felder[neuerFeldIndex].getFigurAufFeld().getTeam() == this.getTeam())
+                continue;
+            if ((Schachbrett.felder[neuerFeldIndex].getFigurAufFeld() != null && Schachbrett.felder[neuerFeldIndex].getFigurAufFeld().getTeam() != this.getTeam())) {
+                gueltigerZug.add(Schachbrett.felder[neuerFeldIndex]);
+                continue;
+            }
+            gueltigerZug.add(Schachbrett.felder[neuerFeldIndex]);
+        }
+        return gueltigerZug;
+    }
+
+
+    private Integer[] getVektoren(){
+        Feld jetzigesFeld =this.getJetzigesFeld();
+        if(jetzigesFeld.getSpalte() == 0){
+            return new Integer[]{-15, -6, 17, 10};
+        }else if(jetzigesFeld.getSpalte() == 1){
+            return new Integer[]{-15, -6, 17, 10, 15, -17};
+        }else if(jetzigesFeld.getSpalte() == 6){
+            return new Integer[]{15, 6, -17, -10, -15, 17};
+        }else if (jetzigesFeld.getSpalte() == 7) {
+            return new Integer[]{15, 6, -17, -10};
+        }
+
+        return new Integer[]{-15, -17, 15, 17, -6, 10, 6, -10};
+    }
 }
+
