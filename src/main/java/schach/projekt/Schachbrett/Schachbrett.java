8f069a2479aa6d220e9d73580dbcf0db39ce120f
Tim Büsdorf <tim090503@gmail.com>
2024-01-21 13:28:35 +0100

    Refactor Schachbrett.java

--- src/main/java/schach/projekt/Schachbrett/Schachbrett.java
+++ src/main/java/schach/projekt/Schachbrett/Schachbrett.java
@@ -1,16 +1,20 @@
-package schach.projekt;
+package schach.projekt.Schachbrett;
 
 import schach.projekt.feld.Feld;
 import schach.projekt.schachFiguren.SchachfigurPanel;
 
+
 import javax.swing.*;
 import java.awt.*;
 
+
 public class Schachbrett implements Runnable {
     private JPanel BrettFenster;
+    private static SchachfigurPanel schachfigurPanel;
     public static final int BREITE = 800;
     public static final int HOEHE = 800;
     public static final Feld[] felder = new Feld[64];
+    private static Feld ausgewaehltesFeld;
 
 
     public JPanel generiereBildschirm(){
@@ -32,18 +36,32 @@ public class Schachbrett implements Runnable {
         showGUI();
     }
 
+    public static void setAusgewaehltesFeld(Feld ausgewaehltesFeld) {
+        Schachbrett.ausgewaehltesFeld = ausgewaehltesFeld;
+    }
+
+    public static Feld getAusgewaehltesFeld() {
+        return ausgewaehltesFeld;
+    }
+
     private void showGUI(){
         JFrame frame = new JFrame("SchachBrett");
         frame.setPreferredSize(new Dimension(BREITE, HOEHE));
         JPanel cp = generiereBildschirm();
-        SchachfigurPanel pp = new SchachfigurPanel();
-        frame.setLayeredPane(pp);
+        schachfigurPanel = new SchachfigurPanel();
+        frame.setLayeredPane(schachfigurPanel);
         frame.setContentPane(cp);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        pp.repaint();
+        schachfigurPanel.repaint();
         frame.setVisible(true);
     }
+
+    public static SchachfigurPanel getSchachfigurPanel() {
+        return schachfigurPanel;
+    }
+
+
 
     public static void main(String[] args) {
         EventQueue.invokeLater(new Schachbrett());
