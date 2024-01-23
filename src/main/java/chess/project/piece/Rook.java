416ad7baaa45d73ba9ece36bb06ecd6e21659411
Tim Büsdorf <tim090503@gmail.com>
2024-01-23 18:58:45 +0100

    Add Rook.java

---
+++ src/main/java/chess/project/piece/Rook.java
@@ -0,0 +1,21 @@
+package chess.project.piece;
+
+import chess.project.Board;
+
+import java.awt.image.BufferedImage;
+
+public class Rook extends Piece{
+
+    public Rook(Board board, int column, int row, boolean isWhite) {
+        super(board);
+        this.column = column;
+        this.row = row;
+        this.xPos = column * board.tileSize;
+        this.yPos = row * board.tileSize;
+
+        this.isWhite = isWhite;
+        this.name = "Rook";
+
+        this.sprite = sheet.getSubimage(4 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
+    }
+}
