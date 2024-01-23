650267cd5fb05f71cab46ac28f5097da0c914f2a
Tim Büsdorf <tim090503@gmail.com>
2024-01-23 18:57:34 +0100

    Add Bishop.java

---
+++ src/main/java/chess/project/piece/Bishop.java
@@ -0,0 +1,21 @@
+package chess.project.piece;
+
+import chess.project.Board;
+
+import java.awt.image.BufferedImage;
+
+public class Bishop extends Piece{
+
+    public Bishop(Board board, int column, int row, boolean isWhite) {
+        super(board);
+        this.column = column;
+        this.row = row;
+        this.xPos = column * board.tileSize;
+        this.yPos = row * board.tileSize;
+
+        this.isWhite = isWhite;
+        this.name = "Bishop";
+
+        this.sprite = sheet.getSubimage(2 * sheetScale, isWhite ? 0 : sheetScale, sheetScale, sheetScale).getScaledInstance(board.tileSize, board.tileSize, BufferedImage.SCALE_SMOOTH);
+    }
+}
