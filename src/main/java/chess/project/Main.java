5cc5e0b85c1f0a2adb0938b914620aeb246e59d1
Tim Büsdorf <tim090503@gmail.com>
2024-01-23 18:58:19 +0100

    Add Main.java

---
+++ src/main/java/chess/project/Main.java
@@ -0,0 +1,20 @@
+package chess.project;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.getContentPane().setBackground(Color.BLACK);
+        jFrame.setLayout(new GridBagLayout());
+        jFrame.setMinimumSize(new Dimension(800, 800));
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Board board = new Board();
+        jFrame.add(board);
+
+        jFrame.setVisible(true);
+    }
+}
