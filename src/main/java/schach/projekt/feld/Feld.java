a3afec5bfeee3d9d436683d50439de9c6532fd8d
Tim Büsdorf <tim090503@gmail.com>
2024-01-23 17:30:58 +0100

    Revert "Delete src/main/java/schach/projekt/feld directory"
    This reverts commit 7c8e75f33a6c2bf14ee5f427a134db84c225a3ac.

---
+++ src/main/java/schach/projekt/feld/Feld.java
@@ -0,0 +1,111 @@
+package schach.projekt.feld;
+
+import schach.projekt.Schachbrett.Schachbrett;
+import schach.projekt.schachFiguren.Schachfiguren;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Feld extends JButton {
+
+    private final int zeile;
+    private final int spalte;
+    private boolean ausgewaehlt;
+    private final int feldId;
+    private final Color origFarbe;
+    private final Color neueFarbe;
+    private boolean kannBewegen;
+    private final FeldVisualisierer feldVisualisierer;
+    private Schachfiguren figurAufFeld;
+
+    public Feld(int spalte, int zeile){
+        this.spalte = spalte;
+        this.zeile = zeile;
+        int index = zeile * 8 + spalte;
+        this.neueFarbe = new Color(218, 196,49);
+        Color blue = new Color(71, 117, 171);
+        Color white = new Color(214, 221, 229);
+        this.setBackground(((index + (double) (index / 8)) % 2 != 0) ? blue : white);
+        this.origFarbe = this.getBackground();
+        this.addActionListener(new FeldAktion(this));
+        this.setBorderPainted(false);
+        this.setOpaque(true);
+
+        this.feldId = index;
+        this.feldVisualisierer = new FeldVisualisierer(this);
+        this.feldVisualisierer.setVisible(false);
+        this.add(new JLabel(String.valueOf(feldId)));
+    }
+    public Schachfiguren getFigurAufFeld() {
+        return figurAufFeld;
+    }
+
+    public void setFigurAufFeld(Schachfiguren figurAufFeld) {
+        this.figurAufFeld = figurAufFeld;
+    }
+
+    public boolean isAusgewaehlt() {
+        return ausgewaehlt;
+    }
+
+    public void setAusgewaehlt(boolean ausgewaehlt) {
+        this.ausgewaehlt = ausgewaehlt;
+        Schachbrett.setAusgewaehltesFeld(((ausgewaehlt) ? this : null));
+        setBackground((ausgewaehlt) ? this.neueFarbe : this.origFarbe);
+        if (ausgewaehlt) {
+            for (Feld gueltigerZug : this.getFigurAufFeld().getGueltigenZug()) {
+                gueltigerZug.machFeldBewegbar();
+            }
+        } else {
+            for (Feld gueltigerZug :this.getFigurAufFeld().getGueltigenZug()) {
+                gueltigerZug.machFeldUnbewegbar();
+            }
+        }
+    }
+    public void entferneFigur(){
+        if(this.getFigurAufFeld() != null){
+            Schachbrett.getSchachfigurPanel().entferneFigurVomBrett(this.getFigurAufFeld());
+        }
+    }
+
+
+    public int getXPos(int breiteProFeld) {
+        return spalte * breiteProFeld;
+    }
+
+    public int getYPos(int laengeProFeld) {
+        return zeile * laengeProFeld;
+    }
+
+    public int getZeile() {
+        return zeile;
+    }
+
+    public int getSpalte() {
+        return spalte;
+    }
+
+    public int getFeldId() {
+        return feldId;
+    }
+
+
+    public boolean isKannBewegen() {
+        return kannBewegen;
+    }
+
+    public FeldVisualisierer getFeldVisualisierer() {
+        return feldVisualisierer;
+    }
+
+    public void machFeldBewegbar() {
+        this.feldVisualisierer.updateDatei();
+        this.feldVisualisierer.setVisible(true);
+        kannBewegen = true;
+    }
+
+    public void machFeldUnbewegbar() {
+        this.feldVisualisierer.setVisible(false);
+        kannBewegen = false;
+    }
+}
---
+++ src/main/java/schach/projekt/feld/FeldAktion.java
@@ -0,0 +1,140 @@
+package schach.projekt.feld;
+
+import schach.projekt.GameManager.GameManager;
+import schach.projekt.GameManager.Rochade;
+import schach.projekt.Schachbrett.Schachbrett;
+import schach.projekt.schachFiguren.SchachfigurPanel;
+import schach.projekt.schachFiguren.Schachfiguren;
+import schach.projekt.schachFiguren.Teams;
+import schach.projekt.schachFiguren.figuren.Bauer;
+import schach.projekt.schachFiguren.figuren.Koenig;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FeldAktion implements ActionListener {
+
+    final Feld feld;
+
+
+    public FeldAktion(Feld feld) {
+        this.feld = feld;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(feld.isKannBewegen()){
+            Feld startfeld = Schachbrett.getAusgewaehltesFeld();
+            boolean koenigBew = startfeld.getFigurAufFeld() instanceof Koenig;
+            bewegeFigur(startfeld);
+            checkEnPassant();
+            if(koenigBew){
+                checkRochade();
+            }
+            Schachbrett.getSchachfigurPanel().repaint();
+            GameManager.progressTurn();
+        }
+        if(feld.getFigurAufFeld() == null && Schachbrett.getAusgewaehltesFeld() != null) {
+            Schachbrett.getAusgewaehltesFeld().setAusgewaehlt(false);
+        }
+        if(feld.getFigurAufFeld() != null && GameManager.getZug() == feld.getFigurAufFeld().getTeam()) {
+            if (feld.getFigurAufFeld() != null && Schachbrett.getAusgewaehltesFeld() != null && !feld.isAusgewaehlt()) {
+                Schachbrett.getAusgewaehltesFeld().setAusgewaehlt(false);
+                feld.setAusgewaehlt(true);
+            } else if (feld.getFigurAufFeld() != null) {
+                feld.setAusgewaehlt(!feld.isAusgewaehlt());
+            }
+        }
+
+         if(istImSchach(Teams.WHITE)) {
+            System.out.println("Weißer König im Schach!");
+            if(istSchachmatt(Teams.WHITE)) {
+                System.out.println("Schachmatt! Spiel vorbei. Schwarz gewinnt");
+            }
+        }
+        if(istImSchach(Teams.BLACK)) {
+            System.out.println("Schwarze König im Schach!");
+            if(istSchachmatt(Teams.BLACK)) {
+                System.out.println("Schachmatt! Spiel vorbei. Weiß gewinnt");
+            }
+        }
+
+
+    }
+
+    public boolean istImSchach(Teams team) {
+        for (Schachfiguren figur : SchachfigurPanel.figuren) {
+            if (figur.getTeam() != team) {
+                ArrayList<Feld> gueltigeZuege = figur.getGueltigenZug();
+                for (Feld zug : gueltigeZuege) {
+                    if (zug.getFigurAufFeld() instanceof Koenig && zug.getFigurAufFeld().getTeam() == team) {
+                        // Der König steht im Schach
+                        return true;
+                    }
+                }
+            }
+        }
+
+        // Der König steht nicht im Schach
+        return false;
+    }
+
+    public boolean istSchachmatt(Teams team) {
+        if (!istImSchach(team)) {
+            return false;
+        }
+        for (Feld feld : Schachbrett.felder) {
+            if (feld.getFigurAufFeld() == null || feld.getFigurAufFeld().getTeam() != team) {
+                Schachfiguren koenig = getKoenig(team);
+                assert koenig != null;
+                Feld jetzigesFeld = koenig.getJetzigesFeld();
+                koenig.bewegeZuEinemNeuemFeld(feld);
+                if (!istImSchach(team)) {
+                    koenig.bewegeZuEinemNeuemFeld(jetzigesFeld);
+                    return false;
+                }
+                koenig.bewegeZuEinemNeuemFeld(jetzigesFeld);
+            }
+        }
+
+        // Kein möglicher Zug, der den König aus dem Schach herausbewegt
+        return true;
+    }
+
+    private Schachfiguren getKoenig(Teams team) {
+        for (Schachfiguren figur : SchachfigurPanel.figuren) {
+            if (figur instanceof Koenig && figur.getTeam() == team) {
+                return figur;
+            }
+        }
+        return null;
+    }
+
+    private void checkEnPassant() {
+        Feld enPassant = GameManager.getEnPassant(feld);
+        if(enPassant != null){
+            enPassant.entferneFigur();
+            enPassant.setFigurAufFeld(null);
+        }
+        GameManager.istEnPassant();
+    }
+
+    private void checkRochade() {
+        Rochade rochade = GameManager.getRochade(feld);
+        if(rochade != null){
+            rochade.turmZurRochade.bewegeZuEinemNeuemFeld(rochade.neuesFeldTurm);
+            GameManager.istRochade();
+        }
+    }
+
+    public void bewegeFigur(Feld startFeld){
+        Schachfiguren figurZumBewegen = Schachbrett.getAusgewaehltesFeld().getFigurAufFeld();
+        feld.entferneFigur();
+
+        startFeld.setAusgewaehlt(false);
+        figurZumBewegen.bewegeZuEinemNeuemFeld(feld);
+        figurZumBewegen.bewegt();
+    }
+}
