4264fdab1a002d806733a689b9669e5713124832
Tim Büsdorf <tim090503@gmail.com>
2024-07-19 15:42:12 +0200

    Build Main() Methode back to normal

--- src/main/java/chess/project/StartChess.java
+++ src/main/java/chess/project/StartChess.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class StartChess {
     
-    void main(String[] args) {
+    public static void main(String[] args) {
         JFrame jFrame = new JFrame();
         jFrame.getContentPane().setBackground(Color.BLACK);
         jFrame.setLayout(new GridBagLayout());
